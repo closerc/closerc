8a421723b5303fa9b6889f653b9d39f0912cceca
closerc <88306883+closerc@users.noreply.github.com>
2023-01-12 11:03:08 +0800

    new exercise

---
+++ Learning/Python_Crash_Course/Chapter_6_dict/exercises/3_glossary.py
@@ -0,0 +1,15 @@
+# 词汇表
+
+glossary = {
+    'print': '打印',
+    'for': '循环',
+    'if': '条件测试',
+    'list': '列表',
+    'dict': '字典',
+}
+
+print("print: " + glossary['print'])
+print("for: " + glossary['for'])
+print("if: " + glossary['if'])
+print("list: " + glossary['list'])
+print("dict: " + glossary['dict'])
