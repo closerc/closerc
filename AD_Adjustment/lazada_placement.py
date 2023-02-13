5b636d6469cf28b46acd11071c08cc28587f4c88
closerc <88306883+closerc@users.noreply.github.com>
2023-02-13 19:43:31 +0800

    new modify

--- AD_Adjustment/lazada_placement.py
+++ AD_Adjustment/lazada_placement.py
@@ -15,65 +15,65 @@ warnings.filterwarnings("ignore")
 
 def lazada_placement(root_path):
     for root, dirs, files in os.walk(root_path):
-        shop_station=root.split("\\")[-1]
+        shop_station = root.split("\\")[-1]
         shop = shop_station.split('-')[0]
         print(shop_station)
-        #print(shop)
+        # print(shop)
         for file in files:
             if '$' not in file:
                 if '--' in file:
                     filename = root_path + '\\' + shop + '\\' + shop_station + '\\' + file
-                    #print(filename)
-                    all_data = pd.read_excel(filename, skiprows=5)
+                    # print(filename)
+                    all_data = pd.read_excel(filename, skiprows=7)
                     SS_data = all_data[all_data['Placement'] == 'Sponsored Search']
                     SP_data = all_data[all_data['Placement'] == 'Sponsored Products']
                     SS_SP_data = all_data[(all_data['Placement'] == 'All - Sponsored Products') | (all_data['Placement'] == 'All - Sponsored Search')]
-                    
-                    SS_path = "D:\lazada直通车\\" + shop + '\\' + shop_station
+
+                    SS_path = r"D:\lazada直通车\\" + shop + '\\' + shop_station
                     if os.path.exists(SS_path):
                         pass
                     else:
                         os.makedirs(SS_path)
-                        
+
                     if SS_data.empty:
                         pass
                     else:
-                        SS_data.to_excel(SS_path + '\\' + file, index = False)
-                    
-                    SP_path = "D:\lazada超级推荐\\" + shop + '\\' + shop_station
+                        SS_data.to_excel(SS_path + '\\' + file, index=False)
+
+                    SP_path = r"D:\lazada超级推荐\\" + shop + '\\' + shop_station
                     if os.path.exists(SP_path):
                         pass
                     else:
                         os.makedirs(SP_path)
-                        
+
                     if SP_data.empty:
                         pass
                     else:
-                        SP_data.to_excel(SP_path + '\\' + file, index = False)          
-    
-                    
-                    SS_SP_path = "D:\lazada全效宝\\" + shop + '\\' + shop_station
+                        SP_data.to_excel(SP_path + '\\' + file, index=False)
+
+                    SS_SP_path = r"D:\lazada全效宝\\" + shop + '\\' + shop_station
                     if os.path.exists(SS_SP_path):
                         pass
                     else:
                         os.makedirs(SS_SP_path)
-                    
+
                     if SS_SP_data.empty:
                         pass
                     else:
-                        SS_SP_data.to_excel(SS_SP_path + '\\' + file, index = False)
-                    
+                        SS_SP_data.to_excel(SS_SP_path + '\\' + file, index=False)
+
                 if 'Business' in file:
                     old_path = root_path + '\\' + shop + '\\' + shop_station + '\\' + file
                     shutil.copy(old_path, SS_path + '\\' + file)
                     shutil.copy(old_path, SP_path + '\\' + file)
                     shutil.copy(old_path, SS_SP_path + '\\' + file)
-                    
-def lazada_zip(start_dir,zip_file):
-    #start_dir要压缩的文件路径
-    #zip_file输出zip文件的路径
+
+
+def lazada_zip(start_dir, zip_file):
+    # start_dir要压缩的文件路径
+    # zip_file输出zip文件的路径
     zip_file = zip_file + '.zip'
-    z = zipfile.ZipFile(zip_file,'w',zipfile.ZIP_DEFLATED)
+    z = zipfile.ZipFile(zip_file, 'w', zipfile.ZIP_DEFLATED)
     # print(z)
     for path, dirname, file_name in os.walk(start_dir):
         # print('文件夹根路径', path)
@@ -81,22 +81,17 @@ def lazada_zip(start_dir,zip_file):
         # print('--去除根路径：', fpath)
         fpath = fpath and fpath + os.sep   # 在原fpath加上\
         # print('**去除根路径+\:', fpath)
-        
-        for filename in file_name: 
-            z.write(os.path.join(path,filename), fpath + filename)
+
+        for filename in file_name:
+            z.write(os.path.join(path, filename), fpath + filename)
     z.close()
     return zip_file
+
 
 if __name__ == "__main__":
     root_path = r'D:\lazada月数据'
     # root_path = 'r' + input('路径（shift+右键，复制为路径）：')
-    lazada_placement(root_path)    
-    lazada_zip('D:\lazada超级推荐','D:\lazada超级推荐')
-    lazada_zip('D:\lazada全效宝','D:\lazada全效宝')
-    lazada_zip('D:\lazada直通车','D:\lazada直通车')
-
-
-            
-                
-        
-    
+    lazada_placement(root_path)
+    lazada_zip(r'D:\lazada超级推荐', r'D:\lazada超级推荐')
+    lazada_zip(r'D:\lazada全效宝', r'D:\lazada全效宝')
+    lazada_zip(r'D:\lazada直通车', r'D:\lazada直通车')
