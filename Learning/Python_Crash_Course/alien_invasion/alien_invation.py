af7e33454ad61a5ce99aa484064db01d2631f6c6
closerc <88306883+closerc@users.noreply.github.com>
2023-02-17 18:18:11 +0800

    new modify

--- Learning/Python_Crash_Course/alien_invasion/alien_invation.py
+++ Learning/Python_Crash_Course/alien_invasion/alien_invation.py
@@ -3,7 +3,6 @@ from pygame.sprite import Group
 
 from settings import Settings
 from ship import Ship
-from alien import Alien
 import game_functions as gf
 
 
@@ -15,21 +14,20 @@ def run_game():
         (ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption("Alien Invation")
 
-    # 创建一艘飞船
+    # 创建一艘飞船、一个子弹编组和一个外星人编组
     ship = Ship(ai_settings, screen)
-
-    # 创建一个用于存储子弹的编组
     bullets = Group()
+    aliens = Group()
 
-    # 创建一个外星人
-    alien = Alien(ai_settings, screen)
+    # 创建外星人群
+    gf.create_fleet(ai_settings, screen, ship, aliens)
 
     # 开始游戏的主循环
     while True:
         gf.check_events(ai_settings, screen, ship, bullets)
         ship.update()
         gf.update_bullet(bullets)
-        gf.update_screen(ai_settings, screen, ship, alien, bullets)
+        gf.update_screen(ai_settings, screen, ship, aliens, bullets)
 
 
 run_game()
